00f5906869f89103719d4fb16f16a816585abf87
leonidlezner <kontakt@lezner.de>
2018-08-12 15:21:11 +0200

    Changing the circles overview layout

--- resources/views/circles/index.blade.php
+++ resources/views/circles/index.blade.php
@@ -23,10 +23,58 @@
     </div>
 
     @if(count($items) > 0)
+        @foreach($items->chunk(3) as $circles)
+            <div class="card-deck circles-overview">
+            @foreach($circles as $item)
+                <div class="card mb-4 @if($item->joined($user)) text-white bg-success @endif" data-uuid="{{ $item->uuid }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{!! $item->link() !!}</h5>
+                        
+                        @if($item->location)
+                            <div><span class="fa fa-map-marker"></span>{{ $item->location }}</div>
+                        @endif
+
+                        <div><span class="fa fa-language"></span>{{ list_languages($item->languages) }}</div>
+                    </div>
+
+                    <div class="card-footer text-center">
+                        <div class="row mb-2">
+                        @for($i = 0; $i < $item->limit; $i++)
+                            <div class="col">
+                                @if($i < count($item->memberships))
+                                <span class="fa fa-user"></span>
+                                @else
+                                <span class="fa fa-circle-o"></span>
+                                @endif
+                            </div>
+                        @endfor
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <small class="d-block">{{ _('Begin') }}</small>
+                                {{ format_date($item->begin) }}
+                            </div>
+                            <div class="col">
+                                <small class="d-block">{{ _('Status') }}</small>
+                                {{ circle_state($item) }}
+                            </div>
+                            <div class="col">
+                                <small class="d-block">{{ _('Presence') }}</small>
+                                {{ translate_type($item->type) }}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            @endforeach
+            </div>
+        @endforeach
+
+        {{-- 
         <table class="table table-striped table-bordered">
             <tr>
                 <th>Circle</th>
-                <th>Title</th>
                 <th>Begin</th>
                 <th>Status</th>
                 <th>Type (virtual/f2f)</th>
@@ -37,13 +85,14 @@
             <tr class="@if($item->joined($user)) font-weight-bold @endif" data-uuid="{{ $item->uuid }}">
                 <td class="align-middle">{!! $item->link($item->id) !!}</td>
                 <td class="align-middle">{!! $item->link($item->title) !!} <div><small>{{ $item->location }}</small></div></td>
-                <td class="align-middle">{{ format_date($item->begin) }}</td>
-                <td class="align-middle">{{ circle_state($item) }}</td>
-                <td class="align-middle">{{ translate_type($item->type) }}</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
                 <td class="align-middle">{{ list_languages($item->languages, 3)}}</td>
             </tr>
             @endforeach
         </table>
+        --}}
 
         @include('inc.pagination')
 
